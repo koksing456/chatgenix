ae4369eda1fe33849640b2ed77bf896d340f1c03
koksingheww <koksing.hew@servicerocket.com>
2023-10-07 00:47:16 +0800

    initial setup local
    Integrate with Landing page
    
    Convert to App router

---
+++ app/components/Title/index.js
@@ -0,0 +1,10 @@
+/**
+
+=========================================================
+** Page Title Exports - Styles can be edit on @stylesheet - styles/core/components/footer
+=========================================================
+
+**/
+
+export * from "./PageTitle";
+export * from "./Title";
